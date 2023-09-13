de3a913d664bed9b66b53fea1463a874de3fbb8d
thepopeofpoland <93283314+thepopeofpoland@users.noreply.github.com>
2023-09-13 15:12:43 -0500

    updated the name from robot to practice

--- Robot.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-    Serial.begin(9600);
-    Serial.print("Hello?\n");
-    Serial.print("whats a matter you?");
-}
-
-void loop() {
-    // put your main code here, to run repeatedly:
-}
