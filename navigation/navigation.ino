753e016002b2040379204ca562f0d607a9add557
thepopeofpoland <93283314+thepopeofpoland@users.noreply.github.com>
2023-10-02 15:46:55 -0500

    initial commit of speaker and nav code.

---
+++ navigation/navigation.ino
@@ -0,0 +1,20 @@
+#include<Servo.h>
+Servo servoLeft;
+Servo servoRight;
+void setup () {
+// Play a tone to show program is starting
+tone(4, 3000, 1000);
+delay(1000);
+// Servos moved to 10 & 11 for more accurate results
+// Try using 12 & 13 as well to see what happens
+servoLeft.attach (11);
+servoRight.attach (10);
+// Full speed forward for 3 seconds
+servoLeft.writeMicroseconds (1800);
+servoRight.writeMicroseconds (1800);
+delay(3000);
+servoLeft.detach ();
+servoRight.detach ();
+}
+void loop () {
+}
