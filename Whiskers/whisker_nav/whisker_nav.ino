1f490a3fa8a18b262ca67bdf7925dbd0bc307f5f
thepopeofpoland <93283314+thepopeofpoland@users.noreply.github.com>
2023-10-13 00:25:15 -0500

    Updated whisker_nav with LEDs

--- Whiskers/whisker_nav/whisker_nav.ino
+++ Whiskers/whisker_nav/whisker_nav.ino
@@ -6,28 +6,27 @@ void setup() {
     pinMode(7, INPUT);
     pinMode(5, INPUT);
 
-    pinMode(8,OUTPUT);
-    pinMode(2,OUTPUT);
-  
+    pinMode(8, OUTPUT);
+    pinMode(2, OUTPUT);
+
     Serial.begin(9600);
 }
 void loop() {
-
     byte whiskerLeft = digitalRead(5);
     byte whiskerRight = digitalRead(7);
 
     Serial.print(whiskerLeft);
     Serial.println(whiskerRight);
 
-    if(whiskerLeft == 0) {
-      digitalWrite(8, HIGH);
-    }else{
-      digitalWrite(8, LOW);
+    if (whiskerLeft == 0) {
+        digitalWrite(8, HIGH);
+    } else {
+        digitalWrite(8, LOW);
     }
-    if(whiskerRight == 0) {
-      digitalWrite(2, HIGH);
-    }else{
-      digitalWrite(2, LOW);
+    if (whiskerRight == 0) {
+        digitalWrite(2, HIGH);
+    } else {
+        digitalWrite(2, LOW);
     }
 
     delay(50);
